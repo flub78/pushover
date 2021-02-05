9214543ace52e135aa78996d7395fd5b0f35268b
Flub78 <frederic.peignot@free.fr>
2021-02-05 18:24:21 +0100

    Add pushover notification

--- status.py
+++ status.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python
 # -*- coding:utf8 -*
 
+"""
+Raspberry watchdog
+
+Send a periodic notification to insure the system is up and running
+"""
+
 import os
+import httplib, urllib
 
-print ("Rasp_alarm status\n")
+conn = httplib.HTTPSConnection("api.pushover.net:443")
+title = "Forest Raspberry status"
 
-uptime = "uptime:\n" + os.system("uptime")
+msg = "uptime:\n" + os.popen("uptime").read()
 
-print(uptime)
-
-print ("bye")
+conn.request("POST", "/1/messages.json",
+  urllib.urlencode({
+    "token": "a7re182g2r6791e5m98whgteyowo7h",
+    "user": "uye7fbag6sqp4ax8oo4itzhosdrnf1",
+    "title": title,
+    "message": msg,
+    "sound": "alien",
+  }), { "Content-type": "application/x-www-form-urlencoded" })
+conn.getresponse()
